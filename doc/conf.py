c762e52839103eccf97aba56a6df84164f0ab6ac
LeoUribC <leo.uribe1205@gmail.com>
2023-10-29 12:51:11 -0500

    fixed typo on extensions

--- doc/conf.py
+++ doc/conf.py
@@ -36,7 +36,7 @@ release = '0.1'
 
 extensions = [ 'sphinx.ext.autodoc', 'sphinx.ext.mathjax',
                'sphinx.ext.autosectionlabel', 'nbsphinx',
-               'jupyter-sphinx.execute' ]
+               'jupyter_sphinx.execute' ]
 
 myst_enable_extensions = ["amsmath", "dollarmath"]
 
